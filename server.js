a4a0529fd51aa24e7c4eda40ab462afd582309e7
liorn <Lior.Negri@nice.com>
2018-02-07 16:54:59 +0200

    routing infra

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 8080;
-var routes = require('node/js/routing');
+var routes = require('../NiceChoice/routing');
 
 app.use(express.static(__dirname));
 app.use('/', routes);
 
 app.listen(port, function(){
-	console.log("started" + port);
+	console.log("started listening on: " + port);
 });
