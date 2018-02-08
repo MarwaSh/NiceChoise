8bf00b37ab9a404f92cff192f1fa785fe05098d9
remahj <Rj123456>
2018-02-08 15:49:58 +0200

    map in comment

--- web-app/googleBusinessCard/googleBusinessCard.component.js
+++ web-app/googleBusinessCard/googleBusinessCard.component.js
@@ -8,15 +8,38 @@ angular
         }
     });
 
-GoogleBusinessCardController.$inject = ['$element'];
-function GoogleBusinessCardController($element) {
+GoogleBusinessCardController.$inject = [];
+function GoogleBusinessCardController() {
 
-    var buildGoogleCard = function (id) {
-        //$element
-        console.log('id: ', id);
-    }
+    /*var initMap = function (id) {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -33.866, lng: 151.196},
+            zoom: 15
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+
+        service.getDetails({
+            placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4' // id
+        }, function (place, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: place.geometry.location
+                });
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                        'Place ID: ' + place.place_id + '<br>' +
+                        place.formatted_address + '</div>');
+                    infowindow.open(map, this);
+                });
+            }
+        });
+    }*/
 
     this.$onInit = function (changesObj) {
-        buildGoogleCard(this.id)
+       // console.log('id: ', this.id);
+        //  initMap(this.id)
     }
 }
