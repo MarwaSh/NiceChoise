39d6b1d911c66d8bead2ae293a15246227ecd1e7
nupharp <Nuphar.Pianko@nice.com>
2018-02-08 18:02:28 +0200

    ui changes + get result

--- web-app/clock/clock.controller.js
+++ web-app/clock/clock.controller.js
@@ -10,7 +10,7 @@ app.controller("clockController", function($scope, $http) {
             callbacks: {
                 stop: function() {
                     $('.message').html('The clock has stopped!')
-                    var url = "" + $scope.topic; //todo: ask Lior...
+                    var url = "/" + $scope.topic + "/votes/"; //todo: ask Lior...
                     // TODO: get results from server according to topic
                     $http.get(url).success(function(response) {
                         alert('the result for ' + $scope.topic + " is: " + response.data);
